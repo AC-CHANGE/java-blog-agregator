4968272099553cd6cbb1806d6b2895fe13241a48
AC-CHANGE <Alexandr.priyma@gmail.com>
2016-03-24 16:25:35 +0200

    Video 11 add users table (All is work)

---
+++ src/main/java/com/test/jba/service/UserService.java
@@ -0,0 +1,19 @@
+package com.test.jba.service;
+
+import com.test.jba.entity.User;
+import com.test.jba.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> findAll(){
+        return userRepository.findAll();
+    }
+}
