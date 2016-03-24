66c269ec559f1c50518aced7c87dd5a294207e76
AC-CHANGE <Alexandr.priyma@gmail.com>
2016-03-24 17:27:13 +0200

    Video 15 Hibernate Lazy initialization (All is work)

--- src/main/java/com/test/jba/entity/User.java
+++ src/main/java/com/test/jba/entity/User.java
@@ -15,7 +15,7 @@ public class User {
     private String email;
     private String password;
 
-    @OneToMany(mappedBy = "user")
+    @OneToMany(mappedBy = "user", fetch=FetchType.LAZY )
     private List<Blog> blogs;
 
     @ManyToMany
